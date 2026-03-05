00f2517e96f21f4380a9f1e25978f595ad62c4e0
Kauhsik Raj <algorithmunloack@gmail.com>
2026-03-05 11:49:56 +0530

    The input of the java

--- Day1/JavaIntro3.java
+++ Day1/JavaIntro3.java
@@ -3,6 +3,21 @@ package Day1 ;
 public class JavaIntro3 {
     public static void main (String[] args) {
         System.out.print("This is test");
+
+        int number = 10;
+        System.out.println("The value of the number is:");
+        System.out.println(number); 
+
+
+        int side = 5;
+        System.out.println("The perimeter of the side is :" + (side*4));
+   
+
+        // If the + sigh is between numbers,it adds 
+        // If the + sigh is between string, it concatenates 
+
+        String name = "Kaushik";
+        System.out.println("My name is " + name); 
         
     }
 }
---
+++ Day1/Javaif.java
@@ -0,0 +1,31 @@
+package  Day1 ;
+
+import java.util.Scanner;
+
+public class Javaif  {
+public static void main (String[] args) {
+
+    // This creates the scanner 
+    Scanner input = new Scanner(System.in); 
+
+    // Prompt the user (optional,but helpful)
+
+    System.out.println("Plese type something");
+   
+    // Capture the user input and stroe it in a string varibale 
+
+    String userInput = input.nextLine();
+
+    System.out.print(userInput);
+
+   input.close();
+
+
+
+   Scanner input1  = new Scanner(System.in);
+
+   System userINput = input1.nextLine();
+
+
+};   
+};
